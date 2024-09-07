cd8f7dcd6d8b71e93e3501b90f317f8e1c7c756f
Mohamad nwilaty <mhmadnwilaty@gmail.com>
2024-09-07 03:52:54 +0300

    finished the game

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 const canvas = document.querySelector("canvas");
 const width= 600 ;
 const height =550 ;
+const score = document.getElementById("score");
+const resetBtn = document.querySelector("button");
+const deathScreen = document.getElementById("resetScreen")
 let xMovment = 25 ;
-let yMovment = 0 ; 
+let yMovment = 0 ;
+let foodX ;
+let foodY;
 canvas.width = width;
 canvas.height = height;
 let cntx = canvas.getContext("2d");
@@ -11,15 +16,18 @@ const unitSize = 25  ;
 const snakeCoulor = "green";
 const foodCoulor = "red";
 let snake = [
-            {x:75 , y:0},
-            {x:50 , y:0},
-            {x:25 , y:0},
+            {x:unitSize* 3 , y:0},
+            {x:unitSize* 2 , y:0},
+            {x:unitSize , y:0},
             {x:0 , y:0}
 ]
 window.addEventListener("keydown" , changeDirection);
-gameTick();
+resetBtn.addEventListener("click" ,()=>{
+    deathScreen.style.display = "none" ;
+    startGame();
+})
 
-drawSnake()
+
 
 function drawSnake(){
     cntx.fillStyle = snakeCoulor; 
@@ -35,17 +43,25 @@ function moveSnake(){
     let head = snake[0];
     let newHead =  {x: head.x + xMovment , y: head.y + yMovment}
     snake.unshift(newHead);
-    snake.pop();
+    if(head.x == foodX && head.y == foodY){
+        score.innerHTML = Number(score.innerHTML) +1
+        createFood()         
+    }
+    else{
+        snake.pop();
+    }
+    
+}
+
+function createFood(){
+    foodX = Math.floor((Math.random() * width) / 25) * 25;
+    foodY = Math.floor((Math.random() * height) / 25) * 25;
+    
 }
 
 function drawFood(){
-    let randomX = Math.floor((Math.random() * width) / 25);
-    let randomY = Math.floor((Math.random() * height) / 25);
-    randomX = randomX * 25 ;
-    randomY = randomY * 25 ;
     cntx.fillStyle = foodCoulor ;
-    console.log(randomX , randomY)
-    cntx.fillRect(randomX , randomY ,unitSize ,unitSize);
+    cntx.fillRect(foodX, foodY ,unitSize ,unitSize);
 }
 
 function changeDirection(e){
@@ -80,13 +96,46 @@ function gameTick(){
     if(running){
         setTimeout(()=>{
            clearCanvas();
+           drawFood()
             moveSnake();
             drawSnake() ;
-            gameTick
-            
-        },75)
+            checkLosing()
+            gameTick()
+    
+        },80)
+    }
+    else{
+        deathScreen.style.display = "flex" ;
     }
 }
 function clearCanvas() {
     cntx.clearRect(0, 0, width, height);
+}
+function checkLosing(){
+    let head = snake[0];
+    if(head.x >= width || head.x < 0){
+        running = false
+    }
+    else if (head.y < 0 || head.y >= height){
+        running = false
+    }
+    for(let i =1 ; i<snake.length ; i++){
+        if(head.x == snake[i].x && head.y == snake[i].y ){
+            running = false
+        }
+    }
+}
+function startGame(){
+    running = true ;
+    snake = [
+        {x:unitSize* 3 , y:0},
+        {x:unitSize* 2 , y:0},
+        {x:unitSize , y:0},
+        {x:0 , y:0}
+        ];
+    xMovment = 25 ;
+    yMovment = 0 ;
+    score.innerHTML = "0" ;
+    createFood();
+    gameTick();
 }
